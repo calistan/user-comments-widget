e423bc6e762b9269c63cbdeb95fe479fd6786292
stanleyhello <stanleyrhe@gmail.com>
2025-08-19 14:53:17 -0700

    Delete unnecessary files

--- dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Starting Feedback Widget Development Server..."
-echo ""
-echo "The demo will be available at: http://localhost:8000"
-echo "Press Ctrl+C to stop the server"
-echo ""
-python3 -m http.server 8000
